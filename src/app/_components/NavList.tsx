af24c0e2011643a807a952732fa24f0303f61280
simuseokyun <sim31059999@gmail.com>
2025-08-09 21:41:05 +0900

    Refactor : 디바이스 별 네비게이션 분리

--- src/app/_components/NavList.tsx
+++ src/app/_components/NavList.tsx
@@ -1,23 +1,49 @@
+'use client';
+import { useState } from 'react';
+import Image from 'next/image';
+import NavItem from './NavItem';
 export default function NavList() {
+    const [active, setActive] = useState('home');
+    const [clickState, setClickState] = useState(false);
+
+    const navItems = [
+        { key: 'home', name: '홈', href: '#home' },
+        { key: 'carrer', name: '경력', href: '#carrer' },
+        { key: 'skill', name: '기술', href: '#skill' },
+        { key: 'project', name: '프로젝트', href: '#project' },
+        { key: 'contact', name: '정보', href: '#contact' },
+    ];
+    const onToggle = () => {
+        if (!clickState) {
+            setClickState(true);
+        } else {
+            setClickState(false);
+        }
+    };
     return (
-        <nav className="fixed top-5 w-full left-1/2 transform -translate-x-1/2 bg-transparent  z-50  h-16 m-2">
-            <div className="max-w-md m-auto">
-                <ul className="flex justify-center gap-10 bg-[rgba(240,240,240,0.7)] backdrop-blur-md p-2 rounded-xl shadow">
-                    <li className="p-1">
-                        <a href="#home" className="text-sm font-semibold  text-sub md:text-base">
-                            홈
-                        </a>
-                    </li>
-                    <li className="p-1">
-                        <a href="#skill" className="text-sm font-semibold text-sub md:text-base">
-                            기술
-                        </a>
-                    </li>
-                    <li className="p-1">
-                        <a href="#project" className="text-sm font-semibold text-sub   md:text-base">
-                            프로젝트
-                        </a>
-                    </li>
+        <nav className="fixed top-5 left-1/2  w-full transform -translate-x-1/2 bg-transparent z-50">
+            <div className="max-w-md m-auto p-0.5  bg-[rgba(240,240,240,0.7)]  shadow rounded-3xl overflow-hidden">
+                <ul className="hidden justify-end items-center gap-5 backdrop-blur-md  md:gap-10 md:justify-center  md:flex  ">
+                    {navItems?.map((item) => (
+                        <NavItem key={item.key} item={item} active={active} onClick={() => setActive(item?.key)} />
+                    ))}
+                </ul>
+                <div className="block items-center gap-5 backdrop-blur-md md:hidden">
+                    {clickState ? (
+                        <Image src="/assets/DropUp.svg" width={30} height={30} alt="image" onClick={onToggle} />
+                    ) : (
+                        <Image src="/assets/DropDown.svg" width={30} height={30} alt="image" onClick={onToggle} />
+                    )}
+                </div>
+            </div>
+
+            <div
+                className={`block max-w-md m-auto  mt-2 bg-[rgba(240,240,240,0.9)] rounded-md transition-all duration-500 ease-in-out overflow-hidden md:hidden ${clickState ? 'h-[180px]' : 'h-0'}`}
+            >
+                <ul className="p-2">
+                    {navItems.map((item) => (
+                        <NavItem key={item.key} item={item} active={active} onClick={() => setActive(item.key)} />
+                    ))}
                 </ul>
             </div>
         </nav>

031e3bb70dd4fc261ad15c23eb9c400e8a3751b0
simuseokyun <sim31059999@gmail.com>
2025-08-09 21:49:18 +0900

    Feat : Data 파일

--- src/app/db/project.ts
+++ src/app/db/project.ts
@@ -1,27 +1,56 @@
-const projectDB = [
+import { ProjectDB } from '../types/type';
+
+const projectDB: ProjectDB[] = [
     {
-        title: '프로젝트1',
-        description: '프로젝트1입니다',
+        id: 1,
+        title: '음악 스트리밍 앱',
+        description:
+            'Spotify Web API를 활용해 사용자의 음악 데이터를 시각화하고,플레이리스트,아티스트 정보 등을 제공, 일정 시간 음악을 스트리밍할 수 있는 리액트 기반 웹 애플리케이션',
+        image: '/assets/project-one.webp',
+        skill: {
+            front: [
+                '/assets/React.svg',
+                '/assets/TypeScript.svg',
+                '/assets/Tailwind.svg',
+                '/assets/Tanstack-Query.png',
+                '/assets/Zustand.webp',
+            ],
+            back: ['/assets/Node.js.svg', '/assets/Express.svg'],
+        },
+        github: 'https://github.com/simuseokyun/music-stream',
+        purpose:
+            '공개 API를 활용한 실질적인 서비스 구축 경험을 목표로 프로젝트를 기획했습니다. 특히, 음악에 대한 개인적인 관심을 바탕으로, 스트리밍 기능을 포함한 음악 애플리케이션을 직접 구현함으로써 프론트엔드와 백엔드 기술을 유기적으로 결합해보는 데 중점을 두었습니다.',
+        features: [
+            'Spotify OAuth 로그인 연동',
+            '특정 트랙, 앨범, 아티스트 검색',
+            '인피니트 스크롤링을 통한 점진적 로딩',
+            '플레이리스트/찜한 앨범 조회/추가/삭제',
+            '플레이리스트 내 트랙 추가/삭제',
+            '아티스트 팔로우/언팔로우',
+            '음악 스트리밍 기능',
+        ],
+        structure: '/assets/MusicStreamStructure.png',
+        process: ['프로젝트 기획', 'API 설계', '프론트엔드 개발', '백엔드 개발', '테스트 및 배포'],
+        troubleShooting: ['문제 해결1'],
     },
     {
-        title: '프로젝트2',
-        description: '프로젝트1입니다',
-    },
-    {
-        title: '프로젝트3',
-        description: '프로젝트1입니다',
-    },
-    {
-        title: '프로젝트4',
-        description: '프로젝트1입니다',
-    },
-    {
-        title: '프로젝트5',
-        description: '프로젝트1입니다',
-    },
-    {
-        title: '프로젝트6',
-        description: '프로젝트1입니다',
+        id: 2,
+        title: '포트폴리오',
+        description: 'Next.js의 SSG 기능을 이용해 성능과 SEO에 최적화된 포트폴리오 사이트',
+        image: '/assets/Portfolio.jpg',
+        skill: { front: ['/assets/Next.js.svg', '/assets/TypeScript.svg', '/assets/Tailwind.svg'], back: [] },
+        github: 'https://github.com/simuseokyun/portfolio',
+        purpose:
+            '문서화하는 것도 의미 있지만, 보다 직관적이고 접근성이 좋은 웹사이트 형태로 제작해보고 싶었습니다. 또한, 이 과정에서 Next.js의 Static Site Generation(SSG) 기능을 활용하여 빠르고 효율적인 페이지 렌더링을 경험하고자 했습니다.',
+        features: [
+            'Next.js의 SSG를 활용한 빠른 페이지 로딩과 SEO 최적화',
+            'Framer Motion을 이용한 부드러운 애니메이션 효과',
+            '반응형 디자인과 웹 접근성 고려',
+        ],
+        structure: '/assets/PortfolioStructure.png',
+
+        process: ['프로젝트 기획', 'API 설계', '프론트엔드 개발', '백엔드 개발', '테스트 및 배포'],
+        troubleShooting: ['문제 해결1'],
     },
 ];
 export default projectDB;
---
+++ src/app/db/skill.ts
@@ -0,0 +1,96 @@
+import { Stack } from '../types/type';
+
+const frontendStackList: Stack[] = [
+    {
+        name: 'HTML5',
+        image: 'HTML5.svg',
+        description: '웹 표준성 및 접근성, 크로스 브라우징을 준수하면서 구조적인 마크업을 작성할 수 있습니다',
+    },
+    {
+        name: 'CSS3',
+        image: 'CSS3.svg',
+        description:
+            '애니메이션을 활용하여 동적인 효과를 구현할 수 있고 디바이스 특성을 고려하여 반응형 웹을 제작할 수 있습니다',
+    },
+    {
+        name: 'Javascript',
+        image: 'JavaScript.svg',
+        description:
+            'DOM 조작 및 이벤트 핸들링을 통해 사용자와의 상호작용을 구현할 수 있고 비동기 통신을 통해 서버와 데이터를 주고받을 수 있습니다',
+    },
+    {
+        name: 'TypeScript',
+        image: 'TypeScript.svg',
+        description: '정적 타입 검사와 타입 추론 기능을 통해 코드의 안정성과 유지보수성을 향상 시킬 수 있습니다',
+    },
+    {
+        name: 'Tailwind',
+        image: 'Tailwind.svg',
+        description: '유틸리티 클래스 기반 스타일링을 통해 빠르게 UI를 구축할 수 있습니다',
+    },
+    {
+        name: 'React',
+        image: 'React.svg',
+        description: '컴포넌트 기반 구조와 상태 관리 기능을 통해 효율적인 UI 개발이 가능합니다',
+    },
+    {
+        name: 'Tanstack Query',
+        image: 'TanstackQuery.png',
+        description: 'API 요청에 대한 캐싱, 리패칭등 기능을 통해 효율적인 데이터 관리를 할 수 있습니다',
+    },
+    {
+        name: 'Next',
+        image: 'Next.js.svg',
+        description:
+            '앱 라우터를 사용하여 효율적인 페이지 관리를 할 수 있고, 서버 사이드 렌더링(SSR)과 정적 사이트 생성(SSG)을 통해 SEO 최적화가 가능합니다',
+    },
+];
+const backendStackList: Stack[] = [
+    {
+        name: 'Node.js',
+        image: 'Node.js.svg',
+        description: '이벤트 기반 논블로킹 구조를 활용해 REST API 서버를 구축할 수 있습니다',
+    },
+    {
+        name: 'Express',
+        image: 'Express.svg',
+        description:
+            '유연한 라우팅 시스템을 통해 REST API 서버를 구축할 수 있으며, 미들웨어를 활용한 요청 처리, 에러 핸들링, 인증 등의 백엔드 기능을 구현할 수 있습니다.',
+    },
+    {
+        name: 'GraphQL',
+        image: 'GraphQL.svg',
+        description:
+            '클라이언트가 필요한 데이터만 선택적으로 요청할 수 있으며, Apollo Client와 함께 사용하여 효율적인 상태 관리와 데이터 캐싱을 구현할 수 있습니다',
+    },
+];
+const etcStackList: Stack[] = [
+    {
+        name: 'Git',
+        image: 'Git.svg',
+        description: 'Git을 통한 버전 형상 관리를 통해 협업 및 코드를 관리할 수 있습니다',
+    },
+    {
+        name: 'github',
+        image: 'GitHub.svg',
+        description: '주기적인 배포를 통해 프로젝트 버전을 관리할 수 있습니다',
+    },
+
+    {
+        name: 'Figma',
+        image: 'Figma.svg',
+        description: '피그마를 통한 인터페이스 설계 및 프로토타입 제작 경험이 있습니다',
+    },
+    {
+        name: 'Vercel',
+        image: 'Vercel.svg',
+        description: 'Vercel을 통한 정적 사이트 배포 및 서버리스 함수 구현이 가능합니다',
+    },
+    {
+        name: 'Socket.io',
+        image: 'Socket.io.svg',
+        description: 'Socket.io를 사용하여 실시간 양방향 통신을 구현할 수 있습니다',
+    },
+];
+
+export { frontendStackList, backendStackList, etcStackList };

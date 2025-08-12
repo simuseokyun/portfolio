21cdd391aed3fd6151ec1dfd19cff652e1bb19cd
simuseokyun <sim31059999@gmail.com>
2025-08-12 21:46:08 +0900

    Refactor : 네비게이션 분리

---
+++ src/app/_components/NavItem.tsx
@@ -0,0 +1,28 @@
+'use client';
+import Link from 'next/link';
+import cx from 'classnames';
+import { NavItem as NavItemProps } from '../types/type';
+export default function NavItem({ item, onClick, active }: NavItemProps) {
+    const { id, name, href } = item;
+    return (
+        <li className="p-1">
+            <Link
+                href={href}
+                onClick={onClick}
+                className="relative text-sm md:text-base font-semibold transition-colors"
+            >
+                <span
+                    className={cx('inline-block', {
+                        'text-black': active === id,
+                        'text-sub': active !== id,
+                    })}
+                >
+                    {name}
+                </span>
+                <span
+                    className={`absolute bottom-[-4px] left-0  h-[3px] bg-[var(--main-color)] transition-all uration-200 rounded-full ${active === id ? 'w-full' : 'w-0'}`}
+                />
+            </Link>
+        </li>
+    );
+}
--- src/app/_components/NavList.tsx
+++ src/app/_components/NavList.tsx
@@ -4,32 +4,31 @@ import Image from 'next/image';
 import NavItem from './NavItem';
 export default function NavList() {
     const [active, setActive] = useState('home');
-    const [clickState, setClickState] = useState(false);
-
+    const [navActive, setNavActive] = useState(false);
     const navItems = [
-        { key: 'home', name: '홈', href: '#home' },
-        { key: 'carrer', name: '경력', href: '#carrer' },
-        { key: 'skill', name: '기술', href: '#skill' },
-        { key: 'project', name: '프로젝트', href: '#project' },
-        { key: 'contact', name: '정보', href: '#contact' },
+        { id: 'home', name: '홈', href: '#home' },
+        { id: 'carrer', name: '경력', href: '#carrer' },
+        { id: 'skill', name: '기술', href: '#skill' },
+        { id: 'project', name: '프로젝트', href: '#project' },
+        { id: 'contact', name: '정보', href: '#contact' },
     ];
     const onToggle = () => {
-        if (!clickState) {
-            setClickState(true);
+        if (!navActive) {
+            setNavActive(true);
         } else {
-            setClickState(false);
+            setNavActive(false);
         }
     };
     return (
-        <nav className="fixed top-5 left-1/2  w-full transform -translate-x-1/2 bg-transparent z-50">
-            <div className="max-w-md m-auto p-0.5  bg-[rgba(240,240,240,0.7)]  shadow rounded-3xl overflow-hidden">
+        <nav className="fixed top-5 left-1/2  w-full transform -translate-x-1/2 bg-transparent z-50 px-4">
+            <div className="max-w-md m-auto p-0.5  bg-[rgba(240,240,240,0.7)]  shadow rounded-3xl overflow-hidden ">
                 <ul className="hidden justify-end items-center gap-5 backdrop-blur-md  md:gap-10 md:justify-center  md:flex  ">
                     {navItems?.map((item) => (
-                        <NavItem key={item.key} item={item} active={active} onClick={() => setActive(item?.key)} />
+                        <NavItem key={item?.id} item={item} active={active} onClick={() => setActive(item?.id)} />
                     ))}
                 </ul>
                 <div className="block items-center gap-5 backdrop-blur-md md:hidden">
-                    {clickState ? (
+                    {navActive ? (
                         <Image src="/assets/DropUp.svg" width={30} height={30} alt="image" onClick={onToggle} />
                     ) : (
                         <Image src="/assets/DropDown.svg" width={30} height={30} alt="image" onClick={onToggle} />
@@ -38,11 +37,11 @@ export default function NavList() {
             </div>
 
             <div
-                className={`block max-w-md m-auto  mt-2 bg-[rgba(240,240,240,0.9)] rounded-md transition-all duration-500 ease-in-out overflow-hidden md:hidden ${clickState ? 'h-[180px]' : 'h-0'}`}
+                className={`block max-w-md m-auto  mt-2  bg-[rgba(240,240,240,0.9)] rounded-md transition-all duration-500 ease-in-out overflow-hidden md:hidden ${navActive ? 'h-[180px]' : 'h-0'}`}
             >
                 <ul className="p-2">
                     {navItems.map((item) => (
-                        <NavItem key={item.key} item={item} active={active} onClick={() => setActive(item.key)} />
+                        <NavItem key={item?.id} item={item} active={active} onClick={() => setActive(item?.id)} />
                     ))}
                 </ul>
             </div>

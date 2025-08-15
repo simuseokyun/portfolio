f570dc5ea325a9d1235c2e9eb10ec31b90d2aaae
simuseokyun <sim31059999@gmail.com>
2025-08-15 17:21:42 +0900

    Feat : 프로젝트 라우터 분리

---
+++ src/app/project/[id]/_components/ProjectDetail.tsx
@@ -0,0 +1,27 @@
+import { ProjectDB } from '@/app/types/type';
+import ProjectPurpose from './ProjectPurpose';
+import ProjectFeature from './ProjectFeature';
+import ProjectSkillBg from './ProjectSkillBg';
+import ProjectUI from './ProjectUI';
+import ProjectStructure from './ProjectStructure';
+import ProjectTrouble from './ProjectTrouble';
+export default function ProjectDetail({ project }: { project?: ProjectDB }) {
+    const {
+        purpose = '',
+        features = [],
+        skillBackground = [],
+        structure = '/assets/CloseButton.svg',
+        design = [],
+        troubleShooting = [],
+    } = project ?? {};
+    return (
+        <div className="mt-5 relative">
+            <ProjectPurpose purpose={purpose} />
+            <ProjectFeature features={features} />
+            <ProjectStructure structure={structure} />
+            <ProjectSkillBg skillBackground={skillBackground} />
+            <ProjectUI design={design} />
+            <ProjectTrouble troubleShooting={troubleShooting} />
+        </div>
+    );
+}
---
+++ src/app/project/[id]/_components/ProjectOverview.tsx
@@ -0,0 +1,28 @@
+import ProjectSkill from './ProjectSkill';
+import Image from 'next/image';
+import { ProjectDB } from '@/app/types/type';
+import Link from 'next/link';
+
+export default function ProjectOverview({ project }: { project?: ProjectDB }) {
+    return (
+        <div className="mt-5 pb-5 border-b-[1px] border-[#e2e2e2] ">
+            <h1 className="font-semibold text-sm">프로젝트 이름</h1>
+            <p className="mt-0.5 text-sm ">{project?.title}</p>
+            <h1 className="font-semibold text-sm mt-3">프로젝트 설명</h1>
+            <p className="mt-0.5 text-sm">{project?.description}</p>
+            <h1 className="font-semibold text-sm mt-3">참여 인원</h1>
+            <p className="mt-0.5 text-sm">1명 </p>
+            <ProjectSkill skill={project?.skill} />
+            <h1 className="font-semibold text-sm mt-3">깃허브 및 사이트 링크</h1>
+
+            <p className="mt-1 flex">
+                <Link href={project?.github || ''} target="_blank">
+                    <Image src="/assets/GitHub.svg" width={20} height={20} alt="깃허브" />
+                </Link>
+                <Link className="ml-2" href={project?.github || ''} target="_blank">
+                    <Image src="/assets/Link.svg" width={20} height={20} alt="깃허브" />
+                </Link>
+            </p>
+        </div>
+    );
+}

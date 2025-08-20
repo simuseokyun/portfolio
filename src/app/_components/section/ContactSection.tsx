a5ca2162190f76506f5ec912de351d3c24d974b6
simuseokyun <sim31059999@gmail.com>
2025-08-20 19:14:06 +0900

    Style

--- src/app/_components/section/ContactSection.tsx
+++ src/app/_components/section/ContactSection.tsx
@@ -13,7 +13,7 @@ export default function ContactSection() {
             <div className="block mt-10 md:flex md:justify-center md:items-center ">
                 <MyImage />
                 <div className=" ml-0 mt-10 md:mt-0 md:ml-15">
-                    <h1 className="text-3xl font-bold  text-center md:text-left  md:text-4xl">Contact Me</h1>
+                    <h1 className="text-3xl font-bold mb-1 text-center md:text-left  md:text-4xl">Contact Me</h1>
                     <p className="text-sub mb-2">성장할 곳을 찾고 있습니다. 편하게 연락주세요</p>
                     <div className="text-center md:text-left">
                         <Link href="/info">

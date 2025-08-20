a44e7a00b90b3a2a4abe76435b5b7d20036a4ca3
simuseokyun <sim31059999@gmail.com>
2025-08-20 19:13:44 +0900

    Style : 패딩 추가

--- src/app/_components/section/ProjectSection.tsx
+++ src/app/_components/section/ProjectSection.tsx
@@ -2,7 +2,12 @@ import ProjectList from '../ProjectList';
 import SectionContainer from '../SectionContainer';
 export default function ProjectSection() {
     return (
-        <SectionContainer id="project" title="프로젝트" description="상세내용을 보려면 클릭해주세요">
+        <SectionContainer
+            id="project"
+            title="프로젝트"
+            description="상세내용을 보려면 클릭해주세요"
+            style="md:px-10 lg:px-0"
+        >
             <ProjectList />
         </SectionContainer>
     );

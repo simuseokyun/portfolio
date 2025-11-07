02dd3d6757bb3db92833d2ad263cd17cea21e6d9
simuseokyun <sim31059999@gmail.com>
2025-11-07 21:41:02 +0900

    Refactor : 데이터 없을 경우 컴포넌트 처리

--- src/app/project/[id]/_components/ProjectStructure.tsx
+++ src/app/project/[id]/_components/ProjectStructure.tsx
@@ -1,13 +1,13 @@
 import Image from 'next/image';
 import { ProjectDB } from '@/app/types/type';
 export default function ProjectStructure({ structure }: Pick<ProjectDB, 'structure'>) {
-    if (!structure?.length) return null;
+    if (!structure) return null;
     return (
         <>
             <h2 className="font-semibold mt-10">&#8226; 구조</h2>
             <div className="w-full left-[-5px]">
                 <Image
-                    src={structure || '/assets/CloseButton.png'}
+                    src={structure}
                     alt="라우팅 구조"
                     width={800}
                     height={600}
